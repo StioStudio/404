f7bbeea4a6ec4e52bd39eed7d36083d8ee0f1efd
stio <dev@stio.studio>
2024-03-03 15:01:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import * as http from 'http';
+import * as fs from 'fs';
+
+http.createServer(function (request, response) {
+    console.log(request.url)
+    switch (request.url) {
+        case "/404/404.js":
+            fs.readFile("./404.js", function (error, content) {
+                response.writeHead(200, { 'Content-Type': "text/javascript" });
+                response.end(content, 'utf-8');
+            });        
+            break;
+
+        case "/404/404.css":
+            fs.readFile("./404.css", function (error, content) {
+                response.writeHead(200, { 'Content-Type': "text/css" });
+                response.end(content, 'utf-8');
+            });        
+            break;
+    
+        default:
+            fs.readFile("./404.html", function (error, content) {
+                response.writeHead(200, { 'Content-Type': "text/html" });
+                response.end(content, 'utf-8');
+            });        
+            break;
+    }
+}).listen(404)
